63409de017d6b831820e927472e6ddbfe63a7752
Sohagkazi <spacevew@gmail.com>
2026-06-14 20:20:05 +0600

    14/06/2026 Photo updae

--- dokan-baki-mobile/android/app/build.gradle.kts
+++ dokan-baki-mobile/android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.dokan_baki_mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
